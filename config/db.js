7b368227969778b47e1d01027a2acde33619272c
Raimundo Pérez <raimundoprez@gmail.com>
2026-02-08 22:29:50 +0100

    Created a server with a vanilla app and a DB connection. Installed the packages required for the project.

--- config/db.js
+++ config/db.js
@@ -1,1 +1,13 @@
-//test
+const mongoose = require("mongoose");
+
+async function dbConnect() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Establecida una conexión a BBDD");
+    }
+    catch(error) {
+        throw new Error("Error conectando a BBDD", {cause: error});
+    }
+}
+
+module.exports = dbConnect;
